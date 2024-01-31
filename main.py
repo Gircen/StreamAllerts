fce5c8079d85554d999f51cf425fb925b3d032c8
antonkolesnikov <girrcen666@icloud.com>
2024-01-31 17:37:26 +0300

    add font file

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import dearpygui.dearpygui as dpg
 
-
+with dpg.font_registry():
+    with dpg.font("resources/NotoSerifCJKjp-Medium.otf", 20, default_font=True) as font:
+        dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
+        dpg.bind_font(font)
